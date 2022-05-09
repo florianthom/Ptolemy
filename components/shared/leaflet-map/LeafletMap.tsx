b9a1e6f18037c924b22ddbc7a4f086cedccd1bc0
unknown <thom.florian.1997@googlemail.com>
2022-05-09 19:19:29 +0200

    Several changes

--- components/shared/leaflet-map/LeafletMap.tsx
+++ components/shared/leaflet-map/LeafletMap.tsx
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLng, LatLngExpression } from "leaflet";
+import { LatLngExpression } from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon, PointExpression } from "leaflet";
 
 type Props = {};
 
 export default function LeafletMap({}: Props) {
   useEffect(() => {}, []);
 
-  const initialCenter: LatLngExpression = [51.505, -0.09];
-  const markerPosition: LatLngExpression = [51.505, -0.09];
+  const initialCenter: LatLngExpression = [
+    52.63109251312334, 13.495667095941126,
+  ];
+  const markerPosition: LatLngExpression = [
+    52.63109251312334, 13.495667095941126,
+  ];
+  console.log(markerIconPng);
 
   return (
     <MapContainer
       center={initialCenter}
-      zoom={13}
+      zoom={17.5}
       scrollWheelZoom={true}
       className="h-96 w-full"
     >
@@ -22,7 +29,20 @@ export default function LeafletMap({}: Props) {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={markerPosition}>
+
+      <Marker
+        position={markerPosition}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng.src,
+            iconSize: [
+              markerIconPng.width,
+              markerIconPng.height,
+            ] as PointExpression,
+            iconAnchor: [markerIconPng.width / 2, markerIconPng.height / 2],
+          })
+        }
+      >
         <Popup>
           A pretty CSS3 popup.
           <br />
