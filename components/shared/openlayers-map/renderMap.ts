0d87af1287d80055ff0930f163804e9391c8474a
unknown <thom.florian.1997@googlemail.com>
2022-05-11 20:34:24 +0200

    Several changes

--- components/shared/openlayers-map/renderMap.ts
+++ components/shared/openlayers-map/renderMap.ts
@@ -3,18 +3,23 @@ import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { RefObject } from "react";
 import "ol/ol.css";
+import { fromLonLat, Projection } from "ol/proj";
+import Interaction, { InteractionOptions } from "ol/interaction/Interaction";
+import { defaults } from "ol/interaction";
+import Draw from "ol/interaction/Draw";
 
 export function renderMap(mapRef: RefObject<HTMLDivElement>): Map {
   const initialMap = new Map({
     target: mapRef.current || undefined,
+    interactions: defaults({ mouseWheelZoom: false }),
     layers: [
       new TileLayer({
         source: new OSM(),
       }),
     ],
     view: new View({
-      center: [0, 0],
-      zoom: 0,
+      center: fromLonLat([13.49566709, 52.6310925]),
+      zoom: 8.5,
     }),
   });
   return initialMap;
