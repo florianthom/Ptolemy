1d8950dcc4dfbfff0f57c22ba38f4eb46f031783
unknown <thom.florian.1997@googlemail.com>
2022-05-13 18:02:24 +0200

    Several changes

--- components/shared/openlayers-map/renderMap.ts
+++ components/shared/openlayers-map/renderMap.ts
@@ -6,21 +6,25 @@ import "ol/ol.css";
 import { fromLonLat, Projection } from "ol/proj";
 import { defaults } from "ol/interaction";
 import Draw from "ol/interaction/Draw";
+import ZoomSlider from "ol/control/ZoomSlider";
 import { get as getProjection } from "ol/proj";
+import { defaults as defaultControls } from "ol/control";
 
 export function renderMap(mapRef: RefObject<HTMLDivElement>): Map {
   const initialMap = new Map({
     target: mapRef.current || undefined,
     interactions: defaults({ mouseWheelZoom: false }),
+    controls: defaultControls().extend([new ZoomSlider()]),
+    view: new View({
+      center: fromLonLat([13.49566709, 52.6310925]),
+      zoom: 8.5,
+      extent: [-572513.341856, 5211017.966314, 916327.095083, 6636950.728974],
+    }),
     layers: [
       new TileLayer({
         source: new OSM(),
       }),
     ],
-    view: new View({
-      center: fromLonLat([13.49566709, 52.6310925]),
-      zoom: 8.5,
-    }),
   });
   return initialMap;
 }
